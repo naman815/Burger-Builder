60b2eb12765d064ba26d69d30724e162ffedab42
Naman <naman815@gmail.com>
2020-11-01 01:31:09 +0530

    Adding axios & sending data to firebase

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React,{Component} from 'react'
 import Aux from '../../../Hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props)=> {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey =>{
-    return <li key={igKey}><span style={{textTransform : 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}</li>
-    });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-    <p><strong>Total Price : ₹ {props.price}</strong></p>
-            <p>Continue to Check Out</p>
-            <Button btnTyp="Danger" clicked={props.purchasedCanceled}>Cancel</Button>
-            <Button btnTyp="Success" clicked={props.purchasedContinued}>Continue</Button>
-        </Aux>
-    )
+class OrderSummary extends Component {
+
+    componentWillUpdate(){
+         console.log("order summary update");
+    }
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey =>{
+        return <li key={igKey}><span style={{textTransform : 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}</li>
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+        <p><strong>Total Price : ₹ {this.props.price}</strong></p>
+                <p>Continue to Check Out</p>
+                <Button btnTyp="Danger" clicked={this.props.purchasedCanceled}>Cancel</Button>
+                <Button btnTyp="Success" clicked={this.props.purchasedContinued}>Continue</Button>
+            </Aux>
+        )
+    }
 }
 
 export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Aux from '../../Hoc/Aux';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES ={
     salad : 20,
@@ -86,7 +87,28 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler=()=>{
-        alert('Successfully purchased');
+        //alert('Successfully purchased');
+        const order ={
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+            customer : {
+                name: 'Naman',
+                address :{
+                    addressLine1 : 'west vinod nagar',
+                    addressLine2 : 'New Delhi',
+                    zipCode : '110092',
+                },
+                email : 'abc@abc.com'
+            },
+            Delivery : 'Take-away'
+        }
+        axios.post('/orders.json',order)
+        .then(res =>{
+            console.log(res);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
     }
 
     render() {
